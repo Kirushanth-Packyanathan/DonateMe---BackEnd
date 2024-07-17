c855b8fe1c59ea15e082825503edf7cf0bdd82d0
T-Luxshan <luckybraveboys@gmail.com>
2024-07-17 21:42:43 +0530

    PaymentRequest DTO added

---
+++ src/main/java/com/example/demo/DTO/PaymentRequest.java
@@ -0,0 +1,16 @@
+package com.example.demo.DTO;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class PaymentRequest {
+    private String name;
+    private String email;
+    private Double amount;
+}
